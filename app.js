4e37ab229f67d67a000b076454757f3023fc5418
msophia1 <msophia1@umbc.edu>
2017-06-06 15:13:22 -0400

    validation commit

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ myApp.controller("mainController", function($scope) {
     */
     $scope.update = function(){
         var txtCoordinates;
-        if(document.getElementById("txtCoordinates").value.length>0){
-            txtCoordinates = document.getElementById("txtCoordinates").value;
-            var array = txtCoordinates.split(',');
+        if($scope.coordinates){
+            var array = $scope.coordinates.split(',');
             //Labels array to stored to maintain the X-asis of the graph which is currenlt a blank array.
             labels = Array(array.length).fill('');
             $scope.draw(array)
-        }        
+        }
+        else{
+            window.alert(" Please provide the input coordinates");
+        }
     }
     /*
         Function to draw the chart.
